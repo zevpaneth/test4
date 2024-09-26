0dbbf07b6a94422f4b0489843fb4309331ae82da
Zev Paneth <z0504167424@gmail.com>
2024-09-26 17:55:35 +0300

    it is not finished!2

--- services/beeperService.ts
+++ services/beeperService.ts
@@ -1,6 +1,6 @@
 import {Beeper, Status} from "../models/beeper.js";
 import {v4 as uuidv4} from 'uuid';
-import {readFromJsonFile, writeBeeperToJsonFile} from "../DAL/jsonBeepers.js";
+import {readFromJsonFile, writeBeeperToJsonFile, updateBeepersToJsonFile} from "../DAL/jsonBeepers.js";
 import beeper from "../routes/beeper";
 
 
@@ -30,4 +30,37 @@ export const bringBeeperById = async (id: string): Promise<Beeper> => {
         throw new Error(`Beeper by id ${id} not found`);
     }
     return beeper;
+}
+
+export const statusUpdateById = async (id: string): Promise<Beeper> => {
+
+    const beeper: Beeper = await bringBeeperById(id);
+    console.log('Im here')
+    let num: number = 0;
+
+    switch (beeper.status?.toString()) {
+        case Status.manufactured.toString():{
+            beeper.status = Status[Status.assembled];
+            console.log(num + 1)
+            break;
+        }
+        case Status.assembled.toString():{
+            beeper.status = Status[Status.shipped];
+            console.log(num +1)
+            break;
+        }
+        case Status.shipped.toString():{
+            beeper.status = Status[Status.deployed];
+            // timing()
+            break;
+        }
+    }
+
+    const beepers: Beeper[] = await readFromJsonFile();
+
+    const updatedBeepers: Beeper[] = beepers.filter(b => b.id !== id);
+    updatedBeepers.push(beeper);
+    await updateBeepersToJsonFile(beeper);
+    return beeper;
+
 }
