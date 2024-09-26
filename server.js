e7079a423d531c42b60d5e08810e675ba1674a0d
Zev Paneth <z0504167424@gmail.com>
2024-09-26 14:52:26 +0300

    Things I forgot to do commit

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import beeperRoutes from './routes/beeper.js';
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+const app = express();
+app.use(cors());
+app.use(express.json()); // Body parser
+app.use('/api/beepers', beeperRoutes); // Auth Router
+app.listen(PORT, () => {
+    console.log(`server is on port ${PORT}`);
+}); // Listening for requests
